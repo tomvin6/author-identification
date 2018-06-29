7afd458e4ab2634aa2036f8b06f0ef566a5d851d
Tom V <tomvin6@gmail.com>
2018-06-29 12:38:29 +0300

    added log loss function, input files, main script

--- src/main.py
+++ src/main.py
@@ -24,7 +24,6 @@ from keras.callbacks import EarlyStopping
 from nltk import word_tokenize
 from nltk.corpus import stopwords
 from src.evaluations.logloss import *
-
 stop_words = stopwords.words('english')
 
 
@@ -49,6 +48,8 @@ xtrain, xtest, ytrain, ytest = train_test_split(train.text.values, y,
 print(xtrain.shape)
 print(xtest.shape)
 
+print("TF-IDF + LOG REG")
+# TF-IDF feature calculations
 tfv = TfidfVectorizer(min_df=3,  max_features=None,
             strip_accents='unicode', analyzer='word',token_pattern=r'\w{1,}',
             ngram_range=(1, 3), use_idf=1,smooth_idf=1,sublinear_tf=1,
@@ -63,10 +64,58 @@ xvalid_tfv = tfv.transform(xtest)
 # Fitting a simple Logistic Regression on TFIDF
 log_reg = LogisticRegression(C=1.0)
 log_reg.fit(xtrain_tfv, ytrain)  # execute train for Log regression model
-#print('Accuracy on the training set: {:.3f}'.format(log_reg.score(xtrain, ytrain)))
-#print('Accuracy on the test set: {:.3f}'.format(log_reg.score(xtest, ytest)))
+
+
 predictions = log_reg.predict_proba(xvalid_tfv)
 preds = log_reg.predict(xvalid_tfv)
-
 print("logloss: %0.3f " % multiclass_logloss(ytest, predictions))
+print("business friendly output: %0.3f" % (np.sum(preds == ytest) / len(ytest)))
+
+# Fitting a simple Naive Bayes on TFIDF
+print("NAIVE BAYES")
+clf = MultinomialNB()
+clf.fit(xtrain_tfv, ytrain)
+predictions = clf.predict_proba(xvalid_tfv)
+print("logloss: %0.3f " % multiclass_logloss(ytest, predictions))
+print("business friendly output: %0.3f" % (np.sum(preds == ytest) / len(ytest)))
+
+
+# WORD COUNT feature
+print("WORD-COUNT + LOG REG")
+ctv = CountVectorizer(analyzer='word',token_pattern=r'\w{1,}',
+            ngram_range=(1, 3), stop_words = 'english')
+# Fitting Count Vectorizer to both training and test sets (semi-supervised learning)
+ctv.fit(list(xtrain) + list(xtest))
+xtrain_ctv =  ctv.transform(xtrain)
+xvalid_ctv = ctv.transform(xtest)
+
+# Fitting a simple Logistic Regression on Counts
+clf = LogisticRegression(C=1.0)
+clf.fit(xtrain_ctv, ytrain)
+preds = clf.predict(xvalid_ctv)
+predictions = clf.predict_proba(xvalid_ctv)
+
+print ("logloss: %0.3f " % multiclass_logloss(ytest, predictions))
+print("business friendly output: %0.3f" % (np.sum(preds == ytest) / len(ytest)))
+
+
+# Apply SVD, I chose 120 components. 120-200 components are good enough for SVM model.
+print("SVM + TFIDF")
+svd = decomposition.TruncatedSVD(n_components=120)
+svd.fit(xtrain_tfv)
+xtrain_svd = svd.transform(xtrain_tfv)
+xvalid_svd = svd.transform(xvalid_tfv)
+
+# Scale the data obtained from SVD. Renaming variable to reuse without scaling.
+scl = preprocessing.StandardScaler()
+scl.fit(xtrain_svd)
+xtrain_svd_scl = scl.transform(xtrain_svd)
+xvalid_svd_scl = scl.transform(xvalid_svd)
+
+# Fitting a simple SVM
+clf = SVC(C=1.0, probability=True) # since we need probabilities
+clf.fit(xtrain_svd_scl, ytrain)
+predictions = clf.predict_proba(xvalid_svd_scl)
+
+print ("logloss: %0.3f " % multiclass_logloss(ytest, predictions))
 print("business friendly output: %0.3f" % (np.sum(preds == ytest) / len(ytest)))
