191760bf38fa53dbb78ef648aa0f7e41d38baa06
Tom V <tomvin6@gmail.com>
2018-07-06 09:58:13 +0300

    statistics for sentence length

--- src/utils/visualizer.py
+++ src/utils/visualizer.py
@@ -9,6 +9,9 @@ path_prefix = ".." + os.sep + ".." + os.sep + "input" + os.sep
 train_df, test_df, sample_df = load_data_sets(path_prefix + "train.csv", path_prefix + "test.csv", None)
 xtrain, xtest, ytrain, ytest = train_vali_split(train_df)
 
+print('average sentence length: %s' % train_df['text'].map(lambda x: len(x.split())).mean())
+print('min sentence length: %s' % train_df['text'].map(lambda x: len(x.split())).min())
+
 # Load the data and create document vectors
 tfv = TfidfVectorizer(min_df=3, max_features=None,
                       strip_accents='unicode', analyzer='word', token_pattern=r'\w{1,}',
