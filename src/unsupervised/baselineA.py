f526ff4e9b720651aa66b5f9e420340bd0d5f65c
Tom V <tomvin6@gmail.com>
2018-08-18 12:13:16 +0300

    added new improved baseline for unsupervised data

--- src/unsupervised/baselineA.py
+++ src/unsupervised/baselineA.py
@@ -8,24 +8,7 @@ from src.utils.input_reader import load_data_sets, train_vali_split
 from sklearn.cluster import KMeans
 from sklearn import metrics
 
-
-class Permutator(object):
-    def __init__(self):
-        import itertools
-        x = itertools.permutations([0, 1, 2])
-        self.permutations = pd.Series(list(x))
-        self.total_permutations = len(self.permutations)
-        self.cur_permutation_index = 0
-
-    def get_permuration_series(self):
-        return pd.Series(self.permutations[self.cur_permutation_index])
-
-    def set_next_permutation(self):
-        self.cur_permutation_index += 1
-
-    def has_more_permurations(self):
-        return self.cur_permutation_index < self.total_permutations
-
+from src.utils.permutator import Permutator
 
 if __name__ == '__main__':
     print("Baseline unsupervised algorithm")
