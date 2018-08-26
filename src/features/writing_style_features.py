8bd73d724b7da2bfc7a823a3b9f763b05620eb8d
aymann <aymann@ebay.com>
2018-08-26 21:30:49 +0300

    arranged code and added essentials for statistics calc

--- src/features/writing_style_features.py
+++ src/features/writing_style_features.py
@@ -4,7 +4,11 @@ import nltk
 import string
 import pandas as pd
 from nltk.corpus import stopwords
-
+nltk.download('maxent_ne_chunker')
+import spacy
+from spacy.lang.en.stop_words import STOP_WORDS
+from src.features.santimant_features import *
+from src.features.pos_tagging import *
 
 def char_count(sentence):
     """function to return number of chracters """
@@ -90,3 +94,41 @@ def get_writing_style_features(train_df):
     train_df_tmp['fraction_verbs'] = train_df_tmp['text'].apply(lambda row: fraction_verbs(row))
 
     return train_df_tmp
+
+def replace_ents(doc):
+    prefix = 'ent__'
+    text = str(doc.doc)
+    for ent in doc.ents:
+        text = text.replace(ent.orth_, prefix + ent.label_)
+    return text
+
+def preprocess_text(author_df):
+    nlp = spacy.load('en')
+    STOP_WORDS.add("'s")
+    STOP_WORDS.add('the')
+    STOP_WORDS.add('a')
+    for word in STOP_WORDS:
+        nlp.vocab[word].is_stop = True
+    doc = author_df.text.apply(nlp)
+
+    # remove stop words and punctuations
+    clean_and_lemmatize = lambda x: ' '.join([t.lemma_ for t in x if not t.is_punct and not t.is_stop])
+    author_df['text_cleaned'] = doc.apply(clean_and_lemmatize)
+
+    # enteties
+    author_df['text_with_entities'] = doc.apply(replace_ents)
+
+    # pos-tag pairs
+    author_df['text_pos_tag_pairs'] = author_df['text'].apply(lambda row: pos_tag_pairs_sentence(row))
+
+    #additional nlp meta features
+    author_df['polarity_of_text'] = author_df['text'].apply(lambda row: get_polarity(row))
+    author_df['punct_cnt'] = doc.apply(lambda x: len([t for t in x if t.is_punct]))
+    author_df['words_cnt'] = doc.apply(lambda x: len([t for t in x if not t.is_punct]))
+    author_df['ents_cnt'] = doc.apply(lambda x: len(x.ents))
+    author_df['noun_chunks_cnt'] = doc.apply(lambda x: len(list(x.noun_chunks)))
+    author_df['fraction_noun'] = author_df['text'].apply(lambda row: fraction_noun(row))
+    author_df['fraction_adj'] = author_df['text'].apply(lambda row: fraction_adj(row))
+    author_df['fraction_verbs'] = author_df['text'].apply(lambda row: fraction_verbs(row))
+
+    return author_df
