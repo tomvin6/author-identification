9c17c62a4157018930cd04dfb20181f801aa462f
Tom V <tomvin6@gmail.com>
2018-08-27 21:10:24 +0300

    adding 2 additional feature to pipeline

--- src/unsupervised/unsupervised-pipeline.py
+++ src/unsupervised/unsupervised-pipeline.py
@@ -36,7 +36,7 @@ if __name__ == '__main__':
 
     tf_idf_3_grams = Pipeline([
                 ('sel', ItemSelector(key='text')),
-                ('tf', TfidfVectorizer(max_features=1500,
+                ('tf', TfidfVectorizer(max_features=1000,
                           strip_accents='unicode', token_pattern=r'\w{1,}',
                           ngram_range=(1, 3), use_idf=1, smooth_idf=1, sublinear_tf=1,
                           stop_words='english')),
@@ -52,7 +52,7 @@ if __name__ == '__main__':
             ])
     tf_idf_5_grams = Pipeline([
                 ('sel', ItemSelector(key='text')),
-                ('tf', TfidfVectorizer(max_features=1000,
+                ('tf', TfidfVectorizer(max_features=1500,
                           strip_accents='unicode', token_pattern=r'\w{1,}',
                           ngram_range=(1, 5), use_idf=1, smooth_idf=1, sublinear_tf=1,
                           stop_words='english')),
